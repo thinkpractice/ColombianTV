7d82cf65d421e5cfa7db5ffa34034255a5a21a11
tjadejong <tjadejong@thinkpractice.nl>
2018-04-03 21:55:36 +0200

    Added partial episode parsing

--- rcn.py
+++ rcn.py
@@ -4,21 +4,35 @@ import os
 
 baseUrl = r"http://www.canalrcn.com/"
 
+def parseUrlContent(url):
+    result = requests.get(url)
+    return BeautifulSoup(result.content, "html.parser")
+
 def programsUrl():
     return os.path.join(baseUrl, "programas")
+
+def episodesUrl(programUrl):
+    return os.path.join(programUrl, "capitulos")
 
 def titleAndLinkFor(programHtml):
     urlTag = programHtml.find("a")
     return urlTag.contents[0], urlTag["href"]
 
+def episodesFor(episodesUrl):
+    parsedEpisodes = parseUrlContent(episodesUrl)
+    episodes = parsedEpisodes.find_all("div", "views-field-title")
+    for episode in episodes:
+        title, url = titleAndLinkFor(episode)
+        print("   {}: {}".format(title, url))
 
-
-result = requests.get(programsUrl())
-soup = BeautifulSoup(result.content, "html.parser")
+soup = parseUrlContent(programsUrl())
 
 programs = soup.find_all("div", "views-field-title") # "views-field-field-imagen-programa")
 for program in programs:
-    print(titleAndLinkFor(program))
+    title, url = titleAndLinkFor(program)
+    print("{}: {}".format(title, url))
+    episodesFor(episodesUrl(url))
+
 
 
 
